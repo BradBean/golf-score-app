c6bb1cf262c49e23c323a63e9466a28f90197151
Brad Bean <bradbean@Brads-MacBook-Pro.local>
2023-03-15 14:55:16 -0600

    had a big redundacy in my code so this commit took a little longer. Fixed it all up also learned about redirectTo. I also finshed the second page and it fades out and redirects you to the course depending on which ever button you clicked

--- golf-score-app/app.js
+++ golf-score-app/app.js
@@ -1,20 +1,40 @@
 window.onload = function() {
-
-
-const firstButton = document.getElementById('find-courses-button');
-const firstScreen = document.getElementById('first-screen');
-const secondScreen = document.getElementById('second-screen');
-
-firstButton.addEventListener('click', function() {
-  firstScreen.style.opacity = 0;
-  firstScreen.style.transition = 'opacity 0.5s ease-in-out';
-
-  // Wait for fade-out to complete, then hide first screen and show second screen
-  setTimeout(function() {
-    firstScreen.style.display = 'none';
-    secondScreen.style.display = 'block';
-    secondScreen.style.opacity = 1;
-    secondScreen.style.transition = 'opacity 0.5s ease-in-out';
-  }, 500);
-});
-}
+    const firstButton = document.getElementById('find-courses-button');
+    const firstScreen = document.getElementById('first-screen');
+    const secondScreen = document.getElementById('second-screen');
+    const buttons = document.querySelectorAll('.course-selection-button');
+  
+    function fadeOut(element) {
+      element.style.opacity = 0;
+      element.style.transition = 'opacity 0.5s ease-in-out';
+    }
+  
+    function showScreen(screen) {
+      screen.style.display = 'block';
+      screen.style.opacity = 1;
+      screen.style.transition = 'opacity 0.5s ease-in-out';
+    }
+  
+    function redirectTo(url) {
+      window.location.href = url;
+    }
+  
+    firstButton.addEventListener('click', function() {
+      fadeOut(firstScreen);
+      setTimeout(function() {
+        firstScreen.style.display = 'none';
+        showScreen(secondScreen);
+      }, 500);
+    });
+  
+    buttons.forEach(function(button) {
+      button.addEventListener('click', function() {
+        fadeOut(secondScreen);
+        setTimeout(function() {
+          redirectTo(button.dataset.url);
+        }, 500);
+      });
+    });
+  
+  }
+  
